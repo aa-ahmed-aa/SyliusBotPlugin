04551fcf5c13f5e5d74bd07202425c47dd1794de
aa-ahmed-aa <ahmedkhaled36@hotmail.com>
2021-03-17 01:50:54 +0200

    init bot plugin

---
+++ src/AhmedkhdSyliusBotPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ahmedkhd\SyliusBotPlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class AhmedkhdSyliusBotPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
